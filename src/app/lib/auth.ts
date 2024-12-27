ec36d0f44573d19fa6e519034b6af4252a2032f3
hissoune <khalidhissoune962@gmail.com>
2024-12-27 14:24:38 +0100

    fix: Updated authentication logic

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,27 +1,23 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
-const JWT_SECRET = process.env.JWT_SECRET || "hgjf";
-
-if (!JWT_SECRET) {
-  throw new Error("Please define the JWT_SECRET environment variable inside .env.local");
-}
+const JWT_SECRET = process.env.JWT_SECRET || "your-secret-key"; 
 
 export function generateToken(payload: object) {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: "1000d" });
 }
 
 export async function verifyToken(token: string) {
+  console.log(token);
   
-  try {
-    const decode =  jwt.verify(token, JWT_SECRET);
-    return decode;
-  } catch (error) {
-    console.error("JWT verification error:");
-    throw error;
-  }
+  // try {
+    const decoded = await jwt.verify(token, JWT_SECRET); 
+    return decoded; 
+  // } catch (error) {
+  //   console.error("JWT verification error:", error);
+  //   throw new Error("Invalid token");
+  // }
 }
-
 
 export async function hashPassword(password: string) {
   const saltRounds = 10;
@@ -29,5 +25,5 @@ export async function hashPassword(password: string) {
 }
 
 export async function comparePassword(password: string, hashedPassword: string) {
-  return bcrypt.compare(password, hashedPassword);
+  return bcrypt.compare(password, hashedPassword); 
 }
