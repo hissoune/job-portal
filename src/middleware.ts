bcb952a8953bfcf254b01f5113baabb7330dfb70
hissoune <khalidhissoune962@gmail.com>
2024-12-27 01:19:28 +0100

    fix: Updated middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,24 @@
 import { NextResponse } from "next/server";
-import { verifyToken } from "./app/lib/auth";
+import {verifyToken} from "./app/lib/auth"
 
-export async function middleware(req: Request) {
-  const token = req.headers.get("Authorization")?.split(" ")[1];
+export async function middleware(req:any) {
+  const token = req.cookies.get("auth_token"); // Retrieve token from cookies
+console.log('token li baghi ',token);
 
   if (!token) {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
-  try {
-    verifyToken(token);
+
+    const decoded = verifyToken(token)
+    console.log("hada decode ",decoded);
+    
+    req.user = decoded; 
+
     return NextResponse.next();
-  } catch (error) {
-    return NextResponse.redirect(new URL("/auth/login", req.url));
-  }
+  
 }
 
 export const config = {
-  matcher: ["/api/jobs/:path*", "/api/applications/:path*"],
+  matcher: ["/dashboard", "/api/protected-endpoint"],
 };
