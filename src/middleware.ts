43458f16afb060bbf397d46e4b2a7eed3296d607
hissoune <khalidhissoune962@gmail.com>
2024-12-27 17:14:50 +0100

    fix: Updated middleware functionality

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,15 @@
 
 
-import { NextResponse } from "next/server";
-import {verifyToken} from "./app/lib/auth"
+import { NextRequest, NextResponse } from "next/server";
+import { verifyToken } from "./app/lib/auth"
+import { cryptoRuntime } from "jose";
 
-export async function middleware(req:any) {
-  const token = req.cookies.get("auth_token")?.value; 
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get("auth_token")?.value;
+
   const unauthRoutes = ["/auth/login", "/auth/register"];
- 
-  if (unauthRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) &&  !token) {
+
+  if (unauthRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) && !token) {
     return NextResponse.next();
   }
 
@@ -17,21 +19,24 @@ export async function middleware(req:any) {
 
 
 
-    if (unauthRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) && token) {
-      return NextResponse.redirect(new URL("/dashboard", req.url));
-    }
+  if (unauthRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) && token) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
 
-    
 
-    return NextResponse.next();
-  
+  const decode = await verifyToken(token);
+  req.user = decode
+
+
+  return NextResponse.next();
+
 }
 
 export const config = {
   matcher: [
     "/dashboard",
-     "/api/protected-endpoint", 
-     "/auth/login",
+    "/api/protected-endpoint",
+    "/auth/login",
     "/auth/register",
   ],
 };
