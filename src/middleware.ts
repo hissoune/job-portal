6f449c5a9ee434e195422207396309ca68c4f3d0
hissoune <khalidhissoune962@gmail.com>
2024-12-27 01:45:24 +0100

    Fix ; trying to fix the decoding of the token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,24 @@
+"use server"
+
 import { NextResponse } from "next/server";
 import {verifyToken} from "./app/lib/auth"
 
 export async function middleware(req:any) {
-  const token = req.cookies.get("auth_token"); // Retrieve token from cookies
+  const token = req.cookies.get("auth_token")?.value; 
 console.log('token li baghi ',token);
 
   if (!token) {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
+  const unauthRoutes = ["/auth/login", "/auth/register"];
+ 
 
-
-    const decoded = verifyToken(token)
+    const decoded =  verifyToken(token)
     console.log("hada decode ",decoded);
-    
+
+    if (unauthRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
     req.user = decoded; 
 
     return NextResponse.next();
@@ -20,5 +26,6 @@ console.log('token li baghi ',token);
 }
 
 export const config = {
-  matcher: ["/dashboard", "/api/protected-endpoint"],
+  matcher: ["/dashboard", "/api/protected-endpoint", "/auth/login",
+    "/auth/register",],
 };
