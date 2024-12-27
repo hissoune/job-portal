8aa779e3bdd0f2c2d219bff78fe8ee0bb732d3fb
hissoune <khalidhissoune962@gmail.com>
2024-12-27 14:24:38 +0100

    fix: Updated middleware for improved functionality

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,37 @@
-"use server"
+
 
 import { NextResponse } from "next/server";
 import {verifyToken} from "./app/lib/auth"
 
 export async function middleware(req:any) {
   const token = req.cookies.get("auth_token")?.value; 
-console.log('token li baghi ',token);
+  const unauthRoutes = ["/auth/login", "/auth/register"];
+ 
+  if (unauthRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) &&  !token) {
+    return NextResponse.next();
+  }
 
   if (!token) {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
-  const unauthRoutes = ["/auth/login", "/auth/register"];
- 
 
-    const decoded =  verifyToken(token)
-    console.log("hada decode ",decoded);
 
-    if (unauthRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
+
+    if (unauthRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) && token) {
       return NextResponse.redirect(new URL("/dashboard", req.url));
     }
-    req.user = decoded; 
+
+    
 
     return NextResponse.next();
   
 }
 
 export const config = {
-  matcher: ["/dashboard", "/api/protected-endpoint", "/auth/login",
-    "/auth/register",],
+  matcher: [
+    "/dashboard",
+     "/api/protected-endpoint", 
+     "/auth/login",
+    "/auth/register",
+  ],
 };
