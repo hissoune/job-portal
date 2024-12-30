569473bd8911dd56d4aa26dae83a4490ce6163ce
hissoune <khalidhissoune962@gmail.com>
2024-12-30 15:30:26 +0100

    chore(types): define type definitions for jobs and related components

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface Job {
+    id: string;
+    title: string;
+    company: string;
+    location: string;
+    description: string;
+    salary: number;
+    date: string;
+    imageUrl: string;
+  }
+  
+  
