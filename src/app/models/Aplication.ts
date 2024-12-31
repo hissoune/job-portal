be8df927841d205194df0bdc504e8d19e7a56d8a
hissoune <khalidhissoune962@gmail.com>
2024-12-31 15:03:15 +0100

    feat(models): update Application model to include additional fields or functionality

--- src/app/models/Aplication.ts
+++ src/app/models/Aplication.ts
@@ -3,9 +3,10 @@ import mongoose, { Schema, model, models } from "mongoose";
 const ApplicationSchema = new Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    jobId: { type: mongoose.Schema.Types.ObjectId, ref: "Job", required: true },
+    jobTitle: { type: String, required: true }, 
     status: { type: String, enum: ["pending", "accepted", "rejected"], default: "pending" },
     note: { type: String },
+    resume: { type: String, required: true }, 
   },
   { timestamps: true }
 );
